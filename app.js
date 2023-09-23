71d1e5925cc130a577c13d65edcc095da2ba84ab
kajal-jaiswal <kajolkumari.9876@gmail.com>
2023-09-23 23:42:40 +0530

    md5 functionality added it is a hashing security type

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ import  express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose, { Schema } from "mongoose";
-import encrypt from "mongoose-encryption";
-
+import md5 from "md5";
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
 
-console.log(process.env.API_KEY);
+console.log(md5("12345"));
 
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB")
@@ -30,7 +29,6 @@ const userSchema =new mongoose.Schema({
 })
 
 
-userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields:["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -51,7 +49,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res)=>{
     const user =new User({
         email : req.body.username ,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     user.save().then(()=>{
         res.render("secrets");
@@ -62,7 +60,7 @@ app.post("/register",(req,res)=>{
     })
     app.post("/login",(req,res)=>{
         const username =req.body.username;
-        const password =req.body.password;
+        const password =md5(req.body.password);
 
         User.findOne({email:username})
         .then((foundUser)=>{
